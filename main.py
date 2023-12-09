c24f3d4de09a1c1649963cef8ac4efb13e577fb5
kundeline <kundeliine@gmail.com>
2023-12-09 22:14:21 +0900

    doing stuff

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> if __name__=='__main__':
+<<<<<<< HEAD
     print('hellooo')
+=======
+    print('hello0')
+>>>>>>> ba11d4f (doing stuff)
